b575dafb149723d7a70075c78e1fbd2b39760233
lamess233 <lynfinitice@gmail.com>
2024-12-23 17:36:35 +0800

    2024-12-23T17:36:32

--- crtest.sh
+++ crtest.sh
@@ -8,12 +8,12 @@ HYSTERIA_WORKDIR="${USER_HOME}/.hysteria"
 HYSTERIA_CONFIG="${HYSTERIA_WORKDIR}/config.yaml"
 HAPROXY_WORKDIR="${USER_HOME}/.haproxy"
 HAPROXY_CONFIG="${HAPROXY_WORKDIR}/etc/haproxy.cfg"
+
 # 定义 crontab 任务
 
 CRON_HYSTERIA="(nohup ${HYSTERIA_WORKDIR}/web server -c ${HYSTERIA_CONFIG} >/dev/null 2>&1 &)"
 CRON_HAPROXY="${HAPROXY_WORKDIR}/sbin/haproxy -f ${HAPROXY_CONFIG}"
 
-# 定义函数来添加 crontab 任务，减少重复代码
 add_cron_job() {
   local job=$1
   (crontab -l 2>/dev/null | grep -F "$job") || (crontab -l 2>/dev/null; echo "$job") | crontab -
@@ -21,7 +21,7 @@ add_cron_job() {
 
 
 if [ -f "$HYSTERIA_CONFIG" ]; then
-  echo "添加 Hysteria 的 crontab 重启任务"
+  echo "添加 crontab 重启任务"
   add_cron_job "@reboot pkill -kill -u $USER && ${CRON_HYSTERIA} && ${CRON_HAPROXY}"
   add_cron_job "*/12 * * * * pgrep -x \"web\" > /dev/null || ${CRON_HYSTERIA}"
   add_cron_job "*/12 * * * * pgrep -x \"haproxy\" > /dev/null || ${CRON_HAPROXY}"
